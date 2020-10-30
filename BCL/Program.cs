9655d6cfa14b431eb157228b9a830433886f5d5b
ccaptcha <alirezanaseri383@yahoo.com>
2020-10-30 17:47:31 +0330

    add logi

--- BCL/Program.cs
+++ BCL/Program.cs
@@ -12,6 +12,30 @@ namespace BCL {
 
         static void Main (string[] args) {
 
+            string image = @"
+ ________  ________  ___          
+|\   __  \|\   ____\|\  \         
+\ \  \|\ /\ \  \___|\ \  \        
+ \ \   __  \ \  \    \ \  \       
+  \ \  \|\  \ \  \____\ \  \____  
+   \ \_______\ \_______\ \_______\
+    \|_______|\|_______|\|_______|
+                                  
+                
+    open source proj at https://github.com/alireza-nasseri/BCL
+
+    active panel :
+    combo_config |combo_view
+    proxy_config | proxy_view
+    rec_config | rec_view
+    repeater
+    req_manager | req_config | req_config
+    res_manager | res_fetch | res_view 
+    var_config | var_config
+
+        
+            ";
+
             var types = Assembly.GetExecutingAssembly ().GetTypes ().Where (t => t.Name[0] == Utilities.Mode_4[0] /*for remove <PrivateImplementationDetails>*/ && t.Name[1] != '_');
             var onProccess = new OnProccess (types);
             onProccess.SetAppsInStaticStorage ();
